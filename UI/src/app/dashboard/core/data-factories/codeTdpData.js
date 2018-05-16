6202c23365424b714c472e559f6e1647433307d8
Sitharthan <6356904+sitharthan@users.noreply.github.com>
2018-05-16 19:35:56 +0530

    Changes for adding test json data for TDP widget

--- UI/src/app/dashboard/core/data-factories/codeTdpData.js
+++ UI/src/app/dashboard/core/data-factories/codeTdpData.js
@@ -9,7 +9,7 @@
         .factory('codeTdpData', codeTdpData);
 
     function codeTdpData($http) {
-        //var testDetailRoute = 'test-data/commit_detail.json';
+        var testDetailRoute = 'test-data/defect_detail.json';
         var caDetailRoute = '/api/defect';
 
         return {
@@ -18,8 +18,7 @@
 
         // get 15 days worth of commit data for the component
         function details(params) {
-            //return $http.get(HygieiaConfig.local ? testDetailRoute : caDetailRoute, { params: params })
-            return $http.get(caDetailRoute, { params: params })
+            return $http.get(HygieiaConfig.local ? testDetailRoute : caDetailRoute, { params: params })
                 .then(function (response) {
                     return response.data;
                 });
